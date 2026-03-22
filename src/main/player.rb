582ffff901fe2c492e9ddf4b304c16c757e5c5f8
Du Van Nguyen <vandu7124@gmail.com>
2026-03-22 15:26:51 +1100

    Implement buy property feature for Player; Add more tests to #move

--- src/main/player.rb
+++ src/main/player.rb
@@ -1,5 +1,5 @@
 class Player
-    attr_reader :name, :position, :money, :properties
+    attr_reader :name, :position, :money
 
     def initialize(name:, position: 0, money: 16)
         @name = name
@@ -9,5 +9,17 @@ class Player
     
     def move(steps, board)
         @position = (@position + steps) % board.length
+    end
+
+    def buy_property(property)
+        if property.price > @money
+            puts "ERROR: #{name} does not have enough money to buy #{property.name}"
+            return false
+        else
+            @money -= property.price
+            property.owner = self
+            puts "DEBUG: #{name} bought #{property.name} for $#{property.price}. Remaining money: $#{@money}"
+            return true
+        end
     end
 end
--- src/tests/player_spec.rb
+++ src/tests/player_spec.rb
@@ -3,7 +3,7 @@ require_relative '../main/player'
 require_relative '../main/board'
 
 describe 'Player' do
-  
+
   board = Board.new
   board.add_square(Square.new(name: 'Go', type: 'go'))
   board.add_square(Square.new(name: 'Mediterranean Avenue', type: 'property', price: 60, colour: 'brown'))
@@ -23,6 +23,40 @@ describe 'Player' do
       player = Player.new(name: 'Bob')
       player.move(2, board)
       expect(player.position).to eq(2)
+    end
+
+    it 'moves player to the beginning of board when reaching the end' do
+      player = Player.new(name: 'Bob')
+      player.move(3, board)
+      expect(player.position).to eq(0)
+    end
+
+    it 'moves player around when moving more than the board length' do
+      player = Player.new(name: 'Bob')
+      player.move(5, board)
+      expect(player.position).to eq(2)
+    end
+  end
+
+  describe '#buy_property' do
+    player = Player.new(name: 'Bob', money: 15)
+    property = Property.new(name: 'Boardwalk', price: 4, colour: 'dark blue')
+
+    it 'buys property if player has enough money' do
+      expect(player.buy_property(property)).to eq(true)
+      expect(property.owner).to eq(player)
+    end
+
+    it 'gives player correct remaining money after buying property' do
+      player.buy_property(property)
+      expect(player.money).to eq(11)
+    end
+
+    it 'does not buy property if player does not have enough money' do
+      player = Player.new(name: 'Bob', money: 1)
+      property = Property.new(name: 'Boardwalk', price: 4, colour: 'dark blue')
+      expect(player.buy_property(property)).to eq(false)
+      expect(property.owner).to be_nil
     end
   end
 end
