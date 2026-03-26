9bb51a7d7a26d266c48daaaac222a2d6a92c2ca4
Du Van Nguyen <vandu7124@gmail.com>
2026-03-26 15:11:48 +1100

    Refactor `GameManager` to handle loading Game Objects and Game Loop

--- src/main/game_manager.rb
+++ src/main/game_manager.rb
@@ -1,20 +1,25 @@
-# frozen_string_literal: true
+# frozen_string_literal: true `
+require_relative 'predefined_dice'
 
 class GameManager
-  def initialize(board_data, players_data)
+  def initialize(board_data, players_data, dice_data)
+    raise ArgumentError, 'ERROR: Board data is needed' if board_data.nil? || board_data.empty?
+    raise ArgumentError, 'ERROR: Players data is needed' if players_data.nil? || players_data.empty?
+
     @board_data = board_data
     @players_data = players_data
+    @dice_data = dice_data
   end
 
   def load_board
     board = Board.new
     @board_data.each do |square|
-      if square["type"] == 'property'
-        board.add_square(Property.new(name: square["name"], price: square["price"], colour: square["colour"]))
-      elsif square["type"] == 'go'
+      if square['type'] == 'property'
+        board.add_square(Property.new(name: square['name'], price: square['price'], colour: square['colour']))
+      elsif square['type'] == 'go'
         board.add_square(Go.new)
       else
-        raise "ERROR: Invalid square type: #{square.type}"
+        raise "ERROR: Invalid square type: #{square['type']}"
       end
     end
     board
@@ -23,14 +28,61 @@ class GameManager
   def load_players
     players = []
     @players_data.each do |player_data|
-      players << Player.new(name: player_data["name"])
+      # Only accepts name of the player
+      raise 'ERROR: Wrong player data format' unless player_data.keys.map(&:to_s) == ['name']
+      raise ArgumentError, "ERROR: Player name can't be empty" if player_data['name'].nil? || player_data['name'].empty?
+
+      players << Player.new(name: player_data['name'])
     end
     players
   end
 
-  # def load_dice_rolls
-  #   dice_rolls.map { |roll| roll.to_i }
-  #   dice_rolls
-  # end
-end
+  def load_dice
+    PredefinedDice.new(rolls_data: @dice_data)
+  end
 
+  def set_up
+    board = load_board
+    players = load_players
+    predefined_dice = load_dice
+    game_event = GameEvent.new
+
+    [board, players, predefined_dice, game_event]
+  end
+
+  def play
+    board, players, predefined_dice, game_event = set_up
+
+    index = 0
+    loop do
+      current_player = players[index % players.length]
+
+      puts "\n--- TURN #{current_player.name} ---"
+
+      times_passed_go = current_player.move(predefined_dice.roll, board)
+      gain_money_passing_go(current_player, times_passed_go)
+
+      current_square = board[current_player.position]
+      current_square.on_land(current_player, board)
+
+      break if game_is_over(current_player, predefined_dice)
+
+      index += 1
+    end
+    game_event.game_over_announcement(players, board)
+  end
+
+  def game_is_over(player, dice)
+    return true if player.is_bankrupt
+    return true if dice.is_end?
+
+    false
+  end
+
+  def gain_money_passing_go(player, passed_go_count)
+    return if passed_go_count == 0 || passed_go_count.nil?
+
+    player.money += passed_go_count
+    puts "GAIN MONEY AT GO: #{player.name} passed 'Go, gained $#{passed_go_count}. Total money: $#{player.money}"
+  end
+end
--- src/main/main.rb
+++ src/main/main.rb
@@ -1,80 +1,25 @@
 # frozen_string_literal: true
 
-require 'json'
-require 'ostruct'
 require_relative 'player'
 require_relative 'board'
 require_relative 'property'
 require_relative 'square'
 require_relative 'game_event'
+require_relative 'cli_parser'
+require_relative 'game_manager'
+require_relative 'data_loader'
+require_relative 'dice'
+require_relative 'predefined_dice'
 require_relative 'go'
 
-game_event = GameEvent.new
+GameEvent.new
+data_loader = DataLoader.new
 
-# Initialise players
-players = [
-  Player.new(name: 'Peter'),
-  Player.new(name: 'Billy'),
-  Player.new(name: 'Charlotte'),
-  Player.new(name: 'Sweedal')
-]
+opts = CLIParser.new.parse(ARGV)
+board_data = data_loader.load_data_from(opts.board_file_path)
+players_data = data_loader.load_data_from(opts.players_file_path)
+dice = data_loader.load_data_from(opts.dice_rolls_file_path)
 
-players.each { |player| puts "PLAYER ADDED: #{player.inspect}" }
+game_manager = GameManager.new(board_data, players_data, dice)
 
-def get_data_from(file_path)
-  file_path = File.expand_path(file_path, __dir__)
-  data = JSON.parse(File.read(file_path), object_class: OpenStruct)
-  raise "ERROR: Failed to load data from #{file_path}" if data.nil?
-
-  puts "DATA: Loaded data from #{file_path}"
-  data
-end
-
-def load_board(data)
-  board = Board.new
-  data.each do |square|
-    if square.type == 'property'
-      board.add_square(Property.new(name: square.name, price: square.price, colour: square.colour))
-    elsif square.type == 'go'
-      board.add_square(Go.new)
-    else
-      raise "ERROR: Invalid square type: #{square.type}"
-    end
-  end
-  board
-end
-
-def gain_money_passing_go(player, passed_go_count)
-  return if passed_go_count == 0 || passed_go_count.nil?
-
-  player.money += passed_go_count
-  puts "GAIN MONEY AT GO: #{player.name} passed 'Go, gained $#{passed_go_count}. Total money: $#{player.money}"
-end
-
-# GAME LOOP
-board = load_board(get_data_from('../data/board.json'))
-dice = get_data_from('../data/rolls_1.json')
-
-dice.each_with_index do |roll, index|
-  raise 'ERROR: Dice rolls must be from 1 - 6' unless (1..6).cover?(roll)
-
-  current_player = players[index % players.length]
-
-  puts "\n--- TURN #{current_player.name} ---"
-
-  # MOVE PLAYER
-  times_passed_go = current_player.move(roll, board)
-  gain_money_passing_go(current_player, times_passed_go)
-
-  current_square = board[current_player.position]
-
-  current_square.on_land(current_player, board)
-
-  # END GAME IF BANKRUPTCY
-  if current_player.is_bankrupt
-    puts "\n#{current_player.name} is bankrupt!"
-    break
-  end
-end
-
-game_event.game_over_announcement(players, board)
+game_manager.play
