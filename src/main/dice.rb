5fa197dde827881ae2eafe66ec57cc4a17f5ae42
Du Van Nguyen <vandu7124@gmail.com>
2026-03-26 15:07:22 +1100

    Refactor `Dice` class and update `PredefinedDice` to inherit and standardize method naming

--- src/main/dice.rb
+++ src/main/dice.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
+
 class Dice
   def roll
+    # Default
+  end
+
+  def is_end?
     # Default
   end
 end
--- src/main/predefined_dice.rb
+++ src/main/predefined_dice.rb
@@ -1,15 +1,18 @@
 # frozen_string_literal: true
 
+require_relative 'dice'
 class PredefinedDice < Dice
-  def initialize(rolls_data:)
-    super
+  def initialize(rolls_data: [])
+    raise ArgumentError, 'ERROR: Roll data is needed' if rolls_data.nil? || rolls_data.empty?
+
     @rolls_data = rolls_data
   end
+
   def roll
     @rolls_data.shift
   end
 
-  def is_last_roll?
+  def is_end?
     @rolls_data.empty?
   end
 end
